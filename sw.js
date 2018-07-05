7017b240928078b4f5a91699a146924bcc5e9f49
Andrey Nazarov <andreynazarov3@yandex.ru>
2018-07-05 23:19:49 +0400

    service worker

--- sw.js
+++ sw.js
@@ -2,38 +2,38 @@ const cacheName = "c1";
 
 // cache site skeleton on install 
 
-// self.addEventListener('install', function (event) {
-//   event.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(
-//         [
-//           '/css/styles.css',
-//           '/data/restaurants.json',
-//           '/index.html',
-//           '/restaurant.html',
-//           '/js/dbhelper.js',
-//           '/js/main.js',
-//           '/js/restaurant_info.js'
-//         ]
-//       );
-//     })
-//   );
-// });
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/css/styles.css',
+          '/data/restaurants.json',
+          '/index.html',
+          '/restaurant.html',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js'
+        ]
+      );
+    })
+  );
+});
 
-// // cache everything else on network requests
+// cache everything else on network requests
 
-// self.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.match(event.request).then(function (response) {
-//         return response || fetch(event.request).then(function(response) {
-//           cache.put(event.request, response.clone());
-//           return response;
-//         });
-//       });
-//     })
-//   );
-// });
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
 
 /*
 
